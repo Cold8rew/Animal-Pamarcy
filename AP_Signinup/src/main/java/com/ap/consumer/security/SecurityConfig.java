71a5ea55f03a7692dc3533ac525ce90840668849
Cold8rew <ghj8617@naver.com>
2022-04-25 13:59:31 +0900

    로그인 접속허용 URL 수정

--- AP_Signinup/src/main/java/com/ap/consumer/security/SecurityConfig.java
+++ AP_Signinup/src/main/java/com/ap/consumer/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.csrf().disable()
 			.authorizeRequests()
 			//로그인 관련 화면 접속 허용
-			.antMatchers("/loginForm","/login","/").permitAll()
+			.antMatchers("/loginForm","/login","/").permitAll() 
 			.antMatchers("/loginForm?error").permitAll()
 			//회원가입 관련 화면 접속 허용
 			.antMatchers("/signup", "/signupForm").permitAll()
